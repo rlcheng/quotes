d382d0060e5572b4bed44239386d559be2472868
Richard Cheng <rlcheng.7@gmail.com>
2015-09-24 15:18:20 -0700

    modified deploy.rb and added new gemset quotes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,9 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :rvm_ruby_version, '2.2.2@quotes'
+set :application, 'quotes'
+set :repo_url, 'git@github.com:rlcheng/quotes.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -35,14 +36,13 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :keep_releases, 5
 
 namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+  desc 'Restart application'
+  task :restart do
+    on roles(:web), in: :sequence, wait: 5 do
+      execute :mkdir, '-p', "#{ release_path }/tmp"
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
-
 end
+
+after 'deploy:publishing', 'deploy:restart'
